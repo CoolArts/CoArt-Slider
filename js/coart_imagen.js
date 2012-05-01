5095528702417d0369460c51e6b022a40f3f30fe
CoolArts <github@coolarts.us>
2012-05-01 09:09:10 +0200

    Empezando en serio

---
+++ js/coart_imagen.js
@@ -0,0 +1,34 @@
+﻿var coart = coart || {};
+
+coart.imagen = {
+	x: 0,
+	y: 0,
+	width: 0,
+	height: 0,
+	posicion: null,
+	velocidad: null,
+	src: null,
+	
+	extender: function (propiedades) {
+		var propiedades, objeto;
+		objeto = Object.create(this);
+		
+		for (propiedad in propiedades) {
+			if (propiedades.hasOwnProperty(propiedad)) {
+				objeto[propiedad] = propiedades[propiedad]
+			}
+		}
+		
+		return obj;
+	},
+	
+	actualizarOver: function (posicion, ancho, ocupado) {
+		if (this.posicion <= posicion) {
+			
+		}
+	},
+	
+	actualizarOut: function (media) {
+		$(this).animate({'left', media*posicion}, 350).dequeue();
+	}
+}
---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+﻿(function () {
+	if(typeof Object.create !== "function") {
+		Object.create = function (o) {
+			function F() {}
+			F.prototype = o;
+			return new F();
+		};
+	}
+	console.log('hello');
+}());
