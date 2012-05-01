5060a22b3b66d0fd0c2fb560e0800ed5b255dfb0
CoolArts <github@coolarts.us>
2012-05-01 20:40:02 +0200

    agregado archivo plugin

---
+++ js/coart_slplug.js
@@ -0,0 +1,30 @@
+﻿jQuery.coArtSld = function (sel, imgs, vel) {
+	var i=0,
+		scripts = ['coart_imagen', 'coart_slider', 'coart_slcore'];
+	
+	if(typeof Object.create !== "function") {
+		Object.create = function (o) {
+			function F() {}
+			F.prototype = o;
+			return new F();
+		};
+	};
+	
+	function cargarScripts () {
+		if (i < scripts.length) {
+			$.getScript('js/'+scripts[i]+'.js', function () {
+				i++;
+				cargarScripts();
+			});
+		}
+		else {iniciar(sel, imgs, vel)};
+	};
+	
+	function iniciar (sel, imgs, vel) {
+		var slider = Object.create(coart.core);
+		slider.iniciar(sel, imgs, vel);
+		return slider;
+	}
+	
+	cargarScripts();
+};
