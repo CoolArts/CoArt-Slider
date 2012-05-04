200958b877c81e12e5685408724b0565b490afd4
CoolArts <github@coolarts.us>
2012-05-04 02:49:14 +0200

    'Versión 1.0 - Basic'

--- js/coart_slplug.js
+++ js/coart_slplug.js
@@ -1,47 +1,71 @@
-﻿jQuery.coArtSld = function (contenidos) {
+﻿//Definimos la función que iniciará nuestro plug-in
+//Le pasaremos la ruta con nuestro archivo .slider
+jQuery.coArtSld = function (contenidos) {
+	//Definimos un array
+	//con todos los scripts a cargar
 	var scripts = [
 		'js/coart_slext.js',
 		'js/coart_slimg.js',
 		'js/coart_slslid.js',
-		'js/coart_slcore.js'
-	],
-		total = 0,
-		script;
+		'js/coart_slcore.js'],
+		
+		//Definimos una varibale total
+		total = 0;
 	
+	//Por cada script definido en nuestro array
 	for (script in scripts) {
+		//Cargamos el script
 		$.getScript(scripts[script], function () {
-			total++;
-			cargar();
+			//cuando cargue sumamos 1 a total
+			total ++;
+			
+			//si total es igual al número de scripts
+			//ejecutamos "iniciar()"
+			if (total == scripts.length) {iniciar()}
 		})
-	}
+	};
 	
+	//Si no existe Object.create como función
+	//Lo creamos
 	if(typeof Object.create !== "function") {
+		//Le pasaremos un objeto "o"
 		Object.create = function (o) {
+			//Definimos una funcíon vacía "F"
 			function F() {}
+			//convertimos "F"
+			//en un prototipo del objeto "o"
 			F.prototype = o;
+			//devolvemos "F"
 			return new F();
 		};
 	};
 	
-	function cargar () {
-		if (total == scripts.length) {
-			iniciar();
-		}
-	}
-	
-	function iniciar () {		
+	//Definimos nuestra función iniciar
+	function iniciar () {
+		//Cargamos nuestro archivo .slider
 		$.getJSON(
 			contenidos,
-			function (datos) {				
+			//recogemos los datos del archivo
+			//que es un objeto con los datos
+			function (datos) {
+				//Definimos x para usarla en el bucle
 				var x,
+					//Creamos una copia del Objeto coart.core
+					//Y lo almacenamos
 					slider = Object.create(coart.core);
 				
+				//Si el objeto recibido,
+				//contiene la propiedad carpeta
 				if (datos.carpeta) {
+					//Por cada imagen en el array imágenes del obejto
 					for (x in datos.imagenes) {
-						datos.imagenes[x].imagen = datos.carpeta+datos.imagenes[x].imagen
+						//Cambiamos su propiedad source
+						//anteponiéndole la ruta de la carpeta
+						datos.imagenes[x].source = datos.carpeta+datos.imagenes[x].source
 					}
 				}
 				
+				//Iniciamos nuestro objeto slider (coart.core)
 				slider.iniciar(datos);
 		});
 	};
--- js/coart_slslid.js
+++ js/coart_slslid.js
@@ -1,36 +1,86 @@
-﻿var coart = coart || {};
+﻿//Configuramos nuestro espacio de nombres
+var coart = coart || {};
 
+/*
+	Definiremos nuestro Objeto slider,
+	lo instanciaremos a través de nuestra función extender
+	desde "coart.core" contendrá un array con las imágenes
+	y se encargará de decir a las imágenes
+	a donde han de moverse
+*/
 coart.slider = {
+	//definimos las propiedades iniciales
+	/*
+		ancho:		Almacenaremos el ancho total del slider,
+					se sacará de su contenedor.
+		imagenes:	Almacenaremos nuestros objetos imagen.
+	*/
 	ancho: null,
 	imagenes: new Array(),
 	
+	//Definimos la propiedad totalImagenes que será una función,
+	//esta función devolverá la cantidad de imágenes
+	//que hay en el array imágenes
 	totalImagenes: function () {
 		return this.imagenes.length;
 	},
 	
+	//Definimos la propiedad nuevaImagen que será una función,
+	//recibirá un objeto imagen y lo insertará en el array imagenes
 	nuevaImagen: function (imagen) {
 		this.imagenes.push(imagen);
 	},
 	
+	//Definimos la propiedad actualizar que será una función
+	//está función se encargará de enviar a cada imagen
+	//de nuestro array la pocición a donde ha de moverse
+	//y recibirá el tamaño de la imagen con over
+	//y el número de la imagen con over
 	actualizar: function (tamImg, objetivo) {
+		//definimos las variables iniciales
+		/*
+			i:			la usaremos en el bucle.
+			reinicio:	Lo estableceremos en true, si en el momento
+						de pasar las posiciones a las imágenes está
+						en false, se le dirá a la imagen que se mueva
+						a su posición original.
+			mediaC:		Almacenaremos lo que ocupa una imagen si hay
+						alguna otra imagen con over.
+		*/
 		var i,
 			reinicio = true,
 			mediaC = (this.ancho-tamImg)/(this.imagenes.length-1);
 		
+		//Comprobamos:
+		//Si el tamaño de la imagen es diferente y mayor que 0
+		//cambiamos reinicio a false
 		if (tamImg != 0 && tamImg > 0) {reinicio = false}
-		else {reinicio = true}
 		
+		//Ahora por cada imagen en nuestro array
 		for (i=1; i<this.totalImagenes(); i++) {
+			//Si reinicio es true
 			if (reinicio) {
+				//Le decimos a las imágenes que vuelvan a su posición original
 				this.imagenes[i].actualizar(this.imagenes[i].origPos)
 			}
+			//Si reinicio es false
 			else {
-				this.imagenes[i].actualizar(discriminar(i, parseInt(objetivo)))
+				//Le decimos a las imágenes su nueva posición,
+				//Usamos la función "discriminar()"
+				//para ver que fórmula hemos de aplicar
+				this.imagenes[i].actualizar(discriminar(i, objetivo))
 			}
 		}
-
+		
+		//Definimos la función discriminar,
+		//le pasaremos el número de la imagen a moverse
+		//y el número de la imagen con el OVER
 		function discriminar (a, b) {
+			//Si la imagen a moverse es inferior o igual
+			//a la imagen con el OVER
 			if (a <= b) {return mediaC*a}
+			
+			//Si no lo es
 			else {return tamImg+(mediaC*(a-1))}
 		}
 	}
